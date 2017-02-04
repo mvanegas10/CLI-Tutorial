3b050717d51ca3e3908d01da7c44466217a50b32
mvanegas10 <m.vanegas10@uniandes.edu.co>
2017-02-04 10:36:23 -0500

    Initial commit

---
+++ manipulation.sh
@@ -0,0 +1,12 @@
+# ls -a			Shows all files and directories in working directory including hidden files (ones started with a dot <.>). The -a is an option, options modifies behaviour of commands.
+# --other options 	-l list all content of directory in long format (access rights, number of child directories and files, username of file's owner, group of file's owner, size in bytes, datetime 
+# 			last modified, name), -t list all content of directory ordered by time when last modified.
+# --multiple options	Multiple options can be used as follows: ls -alt
+# cp <src> <src> <dest> Copies content of source file to destination file. Source file can be specified as * and that will copy every file from de directory specified
+# mv <src> <src> <dest> Moves (renames) content of source file to destination file. Source file can be specified as * and that will copy every file from de directory specified
+# rm <file>		Removes files, using -r before the filename would remove recursively (used to remove directories)
+
+
+
+
+
---
+++ navigation.sh
@@ -0,0 +1,5 @@
+# pwd stands for Print Working Directory
+# ls list all files and directories in working directory
+# cd switches into specific directory (../) returns one level
+# mkdir creates a new directory in current working directory
+# touch creates new file in current working directory
