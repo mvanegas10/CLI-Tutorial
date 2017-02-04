06ef64a8af783d733be7ac7c946cd59964c96d86
mvanegas10 <m.vanegas10@uniandes.edu.co>
2017-02-04 11:17:13 -0500

    Finished 3.Redirection

---
+++ redirection.sh
@@ -0,0 +1,18 @@
+# --standard input (stdin)	Is information inputted into the terminal through the keyboard or input device.
+# --standard output (stdout)   	Is the information outputted after a process is run.
+# --standart error (stderr)	Is an error message outputted by a failed process.
+# echo				
+# cat				Outputs the contents of a file to the terminal.
+# wc <file>			Outputs the number of lines, words, and characters in file
+# sort 				Takes the standard input and orders it alphabetically for the standard output
+# uniq <file>			Filters out adjacent, duplicate lines in a file
+# grep				Stands for Global Regular Expression Print. It searches files for lines that match a pattern and returns the results. It is also case sensitive. -i enables the command to
+#				be case insensitive. -R (stands for recursive) is used to search within a directory. -l gives only the name of the files with matches.
+# sed s/find/replace		Stands for Stream Editor. It accepts standard input and modifies it based on an expression, before displaying it as output data. It is similar to "find and replace".
+# 				This command will only replace the first instance of the find word on a line. Using sed s/find/replace/g, g standing for global, will replace every match in every line.
+# --redirection commands	'>' redirects standard output of a command to a file, overwriting previous content. '>>' redirects standard output of a command to a file, appending new content to old 
+#				content. '<' redirects standard input to a command. '|' redirects standard output of a command to another command.
+
+
+
+
